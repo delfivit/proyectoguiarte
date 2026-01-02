72676d2a78cf3a12d426a6dcad221b6a325ce735
Delfi Vit <delfinavitor@gmail.com>
2026-01-02 20:49:46 -0300

    Update GAS_ENDPOINT to user's latest Apps Script URL

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
    Keep GAS_ENDPOINT empty or set to your Apps Script Web App to store emails in Google Sheets.
 */
 
-const GAS_ENDPOINT = 'https://script.google.com/macros/s/AKfycbw7SlzsdM3OIkePhN6D6dg3piBhmG2LAAbax1L_OPeNbEHjRqIb081rdCi5rc-PPVma/exec'; // Web App URL (actualizado por el usuario)
+const GAS_ENDPOINT = 'https://script.google.com/macros/s/AKfycbyGuznceRUR_ivxa0qSdgUGhEsyBGYoH26VH40JZi-Lq_Cr-BnmCDRH5g87zs2yAqy9/exec'; // Web App URL (actualizado por el usuario)
 
 document.addEventListener('DOMContentLoaded', () => {
   // HERO: add/remove .scrolled on small scroll
